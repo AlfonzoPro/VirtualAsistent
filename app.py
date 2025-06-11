84716d8bbf9225bcfb45935d97b934d3d12b4d3b
AlfonzoPro <alfonzgabriel@gmail.com>
2025-06-10 23:23:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -152,5 +152,7 @@ def transcribe_audio():
             "message": f"Error inesperado: {str(e)}"
         }), 500
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
